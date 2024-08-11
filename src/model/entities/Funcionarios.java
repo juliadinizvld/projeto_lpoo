2826725d19f9588308703d1d4f54bcc2ec83ece0
Dayane <dayanec.dacunha@gmmail.com>
2024-08-11 14:47:30 -0300

    adicionando classes produtos e funcionarios

--- src/model/entities/Funcionarios.java
+++ src/model/entities/Funcionarios.java
@@ -1,5 +1,26 @@
-package model.entities;
+package Model;
 
 public class Funcionarios {
-	
+
+	private int id;
+	private String nome;
+	private String cpf;
+	private String sexo;
+	private String funcao;
+	private String especialidade;
+	private String crm;
+	private String contato;
+	private String endereco;
+
+	public Funcionarios (int id, String nome, String cpf, String sexo, String funcao, String especialidade, String crm, String contato, String endereco) {
+        this.id = id;
+		this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.funcao = funcao;
+        this.especialidade = especialidade;
+        this.crm = crm;
+        this.contato= contato;
+        this.endereco = endereco;
+    }
 }
