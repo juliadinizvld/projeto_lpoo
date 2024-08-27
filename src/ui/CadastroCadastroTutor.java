b886a26b717df7c3f5642c86416705c847228a0f
Dayane <dayanec.dacunha@gmail.com>
2024-08-27 19:19:35 -0300

    Ajuste do limite de caracteres da class CadastroTutor

--- src/ui/CadastroCadastroTutor.java
+++ src/ui/CadastroCadastroTutor.java
@@ -197,8 +197,8 @@ public class CadastroCadastroTutor extends JPanel {
 		((AbstractDocument) campoNome.getDocument()).setDocumentFilter(new FiltroApenasLetras());
 
 		// Adiciona o filtro para limitar o comprimento máximo
-		((PlainDocument) campoNome.getDocument()).setDocumentFilter(new TamanhoMaximoFiltro(50));
-		((PlainDocument) campoEmail.getDocument()).setDocumentFilter(new TamanhoMaximoFiltro(50));
+		((PlainDocument) campoNome.getDocument()).setDocumentFilter(new TamanhoMaximoFiltro(45));
+		((PlainDocument) campoEmail.getDocument()).setDocumentFilter(new TamanhoMaximoFiltro(45));
 
 		String[] sexos = { "Masculino", "Feminino" };
 		JComboBox<String> selectSexo = new JComboBox<String>(sexos);
