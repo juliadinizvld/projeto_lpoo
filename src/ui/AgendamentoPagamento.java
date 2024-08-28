061fb7a9f19a63541b8cd714141b0de81ac3e181
Ana Clara <ana927742@gmail.com>
2024-08-28 17:27:57 -0300

    Adicionado botão/ realizar pagamento em tela AgendamentoPagamento

--- src/ui/AgendamentoPagamento.java
+++ src/ui/AgendamentoPagamento.java
@@ -16,6 +16,7 @@ import com.toedter.calendar.JDateChooser;
 public class AgendamentoPagamento extends JPanel {
 
     private static final long serialVersionUID = 1L;
+    private final JButton btnRealizarPagamento = new JButton("Realizar Pagamento");
 
     /**
      * Create the panel.
@@ -25,33 +26,41 @@ public class AgendamentoPagamento extends JPanel {
 
         JPanel panel = new JPanel();
         panel.setBackground(new Color(255, 255, 255));
-        panel.setBounds(0, 0, 958, 840);
+        panel.setBounds(0, 0, 914, 641);
         add(panel);
         panel.setLayout(null);
 
         JLabel lblEscolhaData = new JLabel("Escolha a data");
         lblEscolhaData.setHorizontalAlignment(SwingConstants.CENTER);
         lblEscolhaData.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        lblEscolhaData.setBounds(216, 30, 253, 25);
+        lblEscolhaData.setBounds(285, 69, 253, 25);
         panel.add(lblEscolhaData);
 
         JDateChooser dateChooser = new JDateChooser();
         dateChooser.setDateFormatString("dd/MM/yyyy");
-        dateChooser.setBounds(265, 123, 159, 25);
+        dateChooser.setBounds(323, 154, 159, 25);
         dateChooser.setPreferredSize(new Dimension(150, 25));
         panel.add(dateChooser);
 
         JLabel lblPagamento = new JLabel(
-                "<html>Para conclusão do agendamento é necessário realizar o <br>pagamento. Acesse o link abaixo para realização do pagamento!</html>");
+                "<html>Para conclusão do agendamento é necessário realizar o pagamento.</html>");
         lblPagamento.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        lblPagamento.setBounds(126, 180, 480, 66);
+        lblPagamento.setBounds(166, 217, 602, 69);
         panel.add(lblPagamento);
 
         JButton btnRetornar = new JButton("← Retornar");
         btnRetornar.setForeground(Color.WHITE);
         btnRetornar.setBackground(new Color(159, 80, 0));
-        btnRetornar.setBounds(291, 272, 120, 30); // Ajuste a posição e o tamanho conforme necessário
+        btnRetornar.setBounds(247, 324, 120, 30); // Ajuste a posição e o tamanho conforme necessário
         panel.add(btnRetornar);
+        btnRealizarPagamento.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnRealizarPagamento.setBounds(480, 324, 123, 30);
+        panel.add(btnRealizarPagamento);
+        btnRealizarPagamento.setForeground(new Color(255, 255, 255));
+        btnRealizarPagamento.setBackground(new Color(159, 80, 0));
 
         btnRetornar.addActionListener(new ActionListener() {
             @Override
